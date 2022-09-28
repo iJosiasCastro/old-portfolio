be5dde1a5f8ef01ea0f44809356c3d7a6a4eb689
Josias Castro <josiascastro2003@gmail.com>
2022-09-28 14:19:41 -0300

    Starting the home page design

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,155 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Layout>
+      <div class="max-w-2xl mx-auto">
+        <div class="h-24"></div>
+        <div class="text-center">
+          <div class="flex flex-col items-center">
+            <div>
+              <img class="rounded-full w-24 " src="https://sebkay.com/img/seb-avatar-2021.webp" alt="">
+            </div>
+            <img class="h-8 w-8 -mr-16 -mt-6 mb-6" src="/svg/argentina_flag.svg" alt="">
+          </div>
+          <div class="max-w-md mx-auto">
+            <h1 class="text-3xl mb-3">👋 Hola, soy Seb</h1>
+            <p class="text-lg">Soy desarrollador de Web Apps, amo la tecnologia y puedo decir que el <i>código es lo mío.</i></p>
+          </div>
+        </div>
+        <div class="h-14"></div>
+      </div>
+
+      <div>
+        <img class="mx-auto -mb-12 sm:-mb-20" src="/img/computers.png" alt="">
+        <div class="bg-blue-700 text-white py-28">
+          <div class="max-w-2xl mx-auto text-center md:text-lg px-2">
+            <p>Aprendí a programar de forma autómata para crear mis propios proyectos, luego me inicié como programador freelancer, trabajé con emprendimientos y empresas, colaborado en muchos grupos de trabajo para conseguir un producto digital de excelente calidad.</p>
+            <br>
+            <p>Soy una persona segura de sí misma, curiosa por naturaleza y siempre estoy trabajando para mejorar mis habilidades.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="max-w-6xl mx-auto px-5 md:px-10">
+        <div class="bg-gray-100 rounded-2xl border border-gray-300 py-8 -mt-16 dark:bg-gray-800 dark:border-gray-600 grid grid-cols-1 md:grid-cols-3">
+          <div class="text-center px-9 text-sm border-b md:border-b-0 md:border-r md:h-full dark:border-gray-600 max-w-sm mx-auto pb-3 md:py-0">
+            <div class="bg-blue-700 rounded-full p-2.5 w-14 h-14 mx-auto mb-2">
+              <svg class="text-white w-full h-full" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21.698 10.658l2.302 1.342-12.002 7-11.998-7 2.301-1.342 9.697 5.658 9.7-5.658zm-9.7 10.657l-9.697-5.658-2.301 1.343 11.998 7 12.002-7-2.302-1.342-9.7 5.657zm0-19l8.032 4.685-8.032 4.685-8.029-4.685 8.029-4.685zm0-2.315l-11.998 7 11.998 7 12.002-7-12.002-7z"/>
+              </svg>
+            </div>
+            <h2 class="text-base md:text-lg font-medium">Frontend</h2>
+            <p>Me especializó en desarrollar Web Apps con Vue y Nuxt; la potencia de estas herramientas es increíble.</p>
+            <div class="my-7">
+              <h5 class="text-blue-600 dark:text-blue-400">Lenguajes:</h5>
+              <p>
+                HTML<br>
+                CSS<br>
+                SASS<br>
+                Javascript ES5/ES6
+              </p>
+            </div>
+            <div class="my-7">
+              <h5 class="text-blue-600 dark:text-blue-400">Frameworks:</h5>
+              <p>
+                Vue<br>
+                Nuxt<br>
+                Bootstrap<br>
+                Tailwind
+              </p>
+            </div>
+            <div class="my-7">
+              <h5 class="text-blue-600 dark:text-blue-400">Herramientas:</h5>
+              <p>
+                Git<br>
+                Wordpress (Elementor Avanzado)
+              </p>
+            </div>
+          </div>
+          <div class="text-center px-9 text-sm border-b md:border-b-0 md:border-r md:h-full dark:border-gray-600 max-w-sm mx-auto pb-3 mt-8 md:my-0">
+            <div class="bg-blue-700 rounded-full p-2 w-14 h-14 mx-auto mb-2">
+              <svg class="text-white w-full h-full" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="m21 4c0-.478-.379-1-1-1h-16c-.62 0-1 .519-1 1v16c0 .621.52 1 1 1h16c.478 0 1-.379 1-1zm-16.5.5h15v15h-15zm2.818 5.865 2.635 1.622-2.641 1.645c-.207.146-.318.378-.318.613 0 .601.682.966 1.182.613l3.511-2.257c.199-.141.318-.369.318-.614 0-.244-.119-.472-.318-.613l-3.505-2.235c-.498-.35-1.182.009-1.182.612 0 .236.111.468.318.614zm9.678 3.873c0-.414-.336-.75-.75-.75h-3.5c-.413 0-.75.336-.75.75s.337.75.75.75h3.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero"/>
+              </svg>
+            </div>
+            <h2 class="text-base md:text-lg font-medium">Backend</h2>
+            <p>El buen diseño de una base de datos  y su modelación es la clave de toda Web App o sitio web.</p>
+            <div class="my-7">
+              <h5 class="text-blue-600 dark:text-blue-400">Lenguajes:</h5>
+              <p>
+                PHP<br>
+                SQL<br>
+              </p>
+            </div>
+            <div class="my-7">
+              <h5 class="text-blue-600 dark:text-blue-400">Frameworks:</h5>
+              <p>
+                Laravel (MVC y Rest API)<br>
+                Firebase
+              </p>
+            </div>
+            <div class="my-7">
+              <h5 class="text-blue-600 dark:text-blue-400">Herramientas:</h5>
+              <p>
+                Git<br>
+                Oracle Cloud<br>
+                Linux<br>
+                Dia (DFD y DER)<br>
+              </p>
+            </div>
+          </div>
+          <div class="text-center px-9 text-sm md:border-r md:h-full dark:border-gray-600 max-w-sm mx-auto mt-8 md:my-0 md:py-0">
+            <div class="bg-blue-700 rounded-full p-3 w-14 h-14 mx-auto mb-2">
+              <svg class="text-white w-full h-full" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5.495 2h16.505v-2h-17c-1.657 0-3 1.343-3 3v18c0 1.657 1.343 3 3 3h17v-20h-16.505c-1.375 0-1.375-2 0-2zm.505 4h14v16h-14v-16z"/>
+              </svg>
+            </div>
+            <h2 class="text-base md:text-lg font-medium">Profesor</h2>
+            <p>Imparto clases de programación, mi meta es ayudar a otros desarrolladores a seguir mejorando.</p>
+            <div class="my-7">
+              <h5 class="text-blue-600 dark:text-blue-400">Estadisticas:</h5>
+              <p>
+                HTML<br>
+                CSS<br>
+                SASS<br>
+                Javascript ES5/ES6
+              </p>
+            </div>
+            <div class="my-7">
+              <h5 class="text-blue-600 dark:text-blue-400">Habilidades:</h5>
+              <p>
+                Seguimiento de tareas<br>
+                Explicación detallada<br>
+                Uso de problemas didácticos<br>
+                Explicación con recursos visuales<br>
+                Ejemplos de código en vivo
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="h-28"></div>
+      <div class="max-w-2xl mx-auto px-2 text-center">
+        <h2 class="text-xl">Mis últimos trabajos</h2>
+        
+        <div class="mt-12">
+          <a href="" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Ver todos los trabajos
+          </a>
+        </div>
+
+      </div>
+      <div class="h-36"></div>
+      <Contact />
+      <div class="h-28"></div>
+    </Layout>
+  </div>
 </template>
 
 <script>
+import Layout from '../components/Layout.vue'
+import Contact from '../components/Contact.vue'
 export default {
+  components: { Layout, Contact },
   name: 'IndexPage'
 }
 </script>
